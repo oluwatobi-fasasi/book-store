95e52ca765429d5ca71567953daad803e34053c9
oluwatobi-fasasi <wasiufasasi79@gmail.com>
2023-07-25 18:14:20 -0400

    Connect redux to react component

--- src/components/BookStore.js
+++ src/components/BookStore.js
@@ -1,24 +1,11 @@
-import React, { useEffect, useState } from 'react';
+// import React, { useEffect, useState } from 'react';
 import BookCase from './BookCase';
 import Form from './Form';
 
 export default function BookStore() {
-  const [books, setBooks] = useState([]);
-
-  useEffect(() => {
-    setBooks([
-      {
-        title: 'Love Bird',
-        author: 'Suarez',
-      },
-    ]);
-  }, []);
-
   return (
     <div>
-      {books.map((book) => (
-        <BookCase key="" book={book} />
-      ))}
+      <BookCase />
       <Form />
     </div>
   );
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { addBook } from '../redux/books/booksSlice';
 
 export default function Form() {
+  const dispatch = useDispatch();
   return (
     <div>
       <section className="mx-44 mt-11">
         <h2>ADD NEW BOOK</h2>
-        <form action="">
+        <form action="" onSubmit={() => dispatch(addBook({ title: '', author: '', id: '' }))}>
           <input type="text" className="border border-grey-900 h-10" />
           <input type="text" className="border border-grey-900 ml-10 h-10" />
           <button type="submit" className="bg-blue-700 p-4 rounded-lg ml-10">ADD BOOK</button>
