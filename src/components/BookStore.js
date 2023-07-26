af76a69e7298e6300ad4cd4988e065587a47300c
oluwatobi-fasasi <wasiufasasi79@gmail.com>
2023-07-26 04:38:16 -0400

    Update form

--- src/components/BookStore.js
+++ src/components/BookStore.js
@@ -1,4 +1,3 @@
-// import React, { useEffect, useState } from 'react';
 import BookCase from './BookCase';
 import Form from './Form';
 
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { addBook } from '../redux/books/booksSlice';
 
 export default function Form() {
   const dispatch = useDispatch();
+  const [author, setAuthor] = useState('');
+  const [title, setTitle] = useState('');
+
+  const submitForm = (e) => {
+    e.preventDefault();
+    dispatch(addBook({ title, author, id: Date.now() }));
+    setTitle('');
+    setAuthor('');
+  };
   return (
     <div>
       <section className="mx-44 mt-11">
         <h2>ADD NEW BOOK</h2>
-        <form action="" onSubmit={() => dispatch(addBook({ title: '', author: '', id: '' }))}>
-          <input type="text" className="border border-grey-900 h-10" />
-          <input type="text" className="border border-grey-900 ml-10 h-10" />
+        <form action="" onSubmit={submitForm}>
+          <input type="text" className="border border-grey-900 h-10" value={author} onChange={(e) => setAuthor(e.target.value)} placeholder="Author" />
+          <input type="text" className="border border-grey-900 ml-10 h-10" value={title} onChange={(e) => setTitle(e.target.value)} placeholder="Title" />
           <button type="submit" className="bg-blue-700 p-4 rounded-lg ml-10">ADD BOOK</button>
         </form>
       </section>
