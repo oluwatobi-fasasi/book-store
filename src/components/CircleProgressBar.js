03a4bb1c38d547cc043e071ca7acaef32427e303
oluwatobi-fasasi <wasiufasasi79@gmail.com>
2023-08-04 05:44:08 -0400

    update

--- src/components/CircleProgressBar.js
+++ src/components/CircleProgressBar.js
@@ -1,44 +1,3 @@
-// import React from 'react';
-// import PropTypes from 'prop-types';
-
-// const CircleProgressBar = ({ progress }) => {
-//   // Logic to calculate the progress percentage and adjust circle properties
-//   const radius = 50; // Adjust this value to set the size of the circle
-//   const circumference = 2 * Math.PI * radius;
-//   //   const offset = (progress / 100) * circumference;
-//   const offset = circumference - (progress / 100) * circumference;
-
-//   return (
-//     <svg className="circle-progress" width={radius * 2} height={radius * 2}>
-//       <circle
-//         className="circle-background"
-//         cx={radius}
-//         cy={radius}
-//         r={radius - 4}
-//         fill="transparent"
-//         strokeWidth="8" // Adjust this value to set the thickness of the circle
-//         stroke="#e4e4e4" // Adjust this value to set the color of the circle background
-//       />
-//       <circle
-//         className="circle-fill"
-//         cx={radius}
-//         cy={radius}
-//         r={radius - 4}
-//         fill="transparent"
-//         strokeWidth="8" // Adjust this value to set the thickness of the circle
-//         stroke="#0290ff" // Adjust this value to set the color of the filled circle
-//         strokeDasharray={circumference}
-//         strokeDashoffset={offset}
-//       />
-//     </svg>
-//   );
-// };
-
-// CircleProgressBar.propTypes = {
-//   progress: PropTypes.number.isRequired,
-// };
-
-// export default CircleProgressBar;
 import React from 'react';
 import PropTypes from 'prop-types';
 
@@ -46,11 +5,8 @@ const CircleProgressBar = ({ progress }) => {
   const radius = 50;
   const circumference = 2 * Math.PI * radius;
   const offset = (progress / 100) * circumference;
+  const initialRotation = -90;
 
-  // Calculate the initial rotation angle based on the desired starting point
-  const initialRotation = -90; // Default starting point is top center (12 o'clock)
-
-  // Calculate the final rotation angle based on the progress
   const finalRotation = initialRotation + (270 * progress) / 100;
 
   return (
@@ -74,17 +30,16 @@ const CircleProgressBar = ({ progress }) => {
         stroke="#e4e4e4"
         strokeDasharray={circumference}
         strokeDashoffset={offset}
-        // Apply the initial rotation to start the blue color from a specific point
         transform={`rotate(${initialRotation} ${radius} ${radius})`}
       >
-        {/* Apply an additional rotation to ensure the blue color fills clockwise */}
+
         <animateTransform
           attributeName="transform"
           type="rotate"
           from={`${initialRotation} ${radius} ${radius}`}
           to={`${finalRotation} ${radius} ${radius}`}
-          dur="1s" // You can adjust the duration of the animation if needed
-          fill="freeze" // The animation will end in its final state
+          dur="1s"
+          fill="freeze"
         />
       </circle>
     </svg>
